442aceb1d927fbfe2c8440cd979ba8fe67aac7d8
SIGMA <sigma.tcc6924@gmail.com>
2026-09-10 20:51:44 -0300

    feat: adiciona pipeline de CI

---
+++ sigma-tcc/src/test/exemplo.test.jsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("Teste inicial do SIGMA", () => {
+    it("deve executar corretamente", () => {
+        expect(1 + 1).toBe(2);
+    });
+});
